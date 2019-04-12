00fea8dea22f25c806b8d380f02f2a7be2e3cc65
Jose7198 <jose719810@hotmail.com>
2019-04-12 18:01:11 -0500

    Property binding

--- 01-Angular/galeria/src/app/app.component.ts
+++ 01-Angular/galeria/src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Floreria';
+
+  arregloFlores = [
+    new Flor("Papitas","A lo bestia"),
+    new Flor("Carnitas","Gorditas")
+  ];
+
+  arregloFloresJS = [
+    {
+      nombre:"Papitas",
+      descripcion:"A lo bestia"
+    },
+    {
+      nombre:"Carnitas",
+      descripcion:"Gorditas"
+    }
+  ]
+
+  cambioChela(evento:boolean){
+    console.log("Llego a chela: ", evento)
+  }
+
+  cambioBiela(evento:boolean){
+    console.log("Llego a biela: ", evento)
+  }
+
+}
+
+class Flor{
+    
+  constructor(
+    public nombre:string,
+    public descripcion:string
+  ){   
+  }
+
 }
--- 01-Angular/galeria/src/app/item-galeria/item-galeria.component.ts
+++ 01-Angular/galeria/src/app/item-galeria/item-galeria.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-item-galeria',
@@ -15,6 +16,18 @@ export class ItemGaleriaComponent implements OnInit {
   @Input()
   nombreItem;
 
+  @Output()
+  cambioChela:EventEmitter<boolean> = new EventEmitter()
+
+  @Output()
+  cambioBiela:EventEmitter<boolean> = new EventEmitter()
+
+  url = "https://http2.mlstatic.com/biela-de-motor-original-peugeot-207-14-nafta-D_NQ_NP_782765-MLA26076182164_092017-F.jpg"
+
+  notas = [1,2,3,4,5,6,7,8,9,10];
+
+  color = "rojo"
+
   constructor() { }
 
   ngOnInit() {
@@ -23,6 +36,26 @@ export class ItemGaleriaComponent implements OnInit {
   alertar(){
     alert('Auxilio me desmayo ' + this.nombreItem);
   }
+
+  alertarBlur(){
+    alert('Alertar blur')
+  }
+
+  cambiarImagen(){
+    const bielas = "https://http2.mlstatic.com/biela-de-motor-original-peugeot-207-14-nafta-D_NQ_NP_782765-MLA26076182164_092017-F.jpg"
+    const chelas = "https://media-cdn.tripadvisor.com/media/photo-s/09/0f/ad/dd/chelas.jpg"
+    if (this.url == bielas){
+      this.url = chelas
+      this.cambioChela.emit(true);
+      this.color= "chela";
+    }else{
+      this.url = bielas
+      this.cambioBiela.emit(true);
+      this.color = "biela"
+  }
+
+
+}
 
 }
 
